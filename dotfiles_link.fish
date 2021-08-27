09faab83ba9290d4987df109d24c53f22e599d06
Toshichika Mashimo <mashita1023@gmail.com>
2021-08-27 21:34:11 +0900

    create symboriclick

---
+++ dotfiles_link.fish
@@ -0,0 +1,6 @@
+ln -s ~/dotfiles/.Brewfile ~/.Brewfile
+ln -s ~/dotfiles/fish/ ~/.config/fish
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.leaf.d ~/.emacs.d
+ln -s ~/dotfiles/.tmux ~/.tmux
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
